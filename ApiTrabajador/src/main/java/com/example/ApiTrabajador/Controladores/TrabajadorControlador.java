5a8f0668e191909d5f75a4c9a0e977c450237753
Cesar Llanos <cesardavidllanos@gmail.com>
2021-12-07 16:08:00 -0500

    agrego el crossorigins

--- ApiTrabajador/src/main/java/com/example/ApiTrabajador/Controladores/TrabajadorControlador.java
+++ ApiTrabajador/src/main/java/com/example/ApiTrabajador/Controladores/TrabajadorControlador.java
@@ -7,15 +7,18 @@ import com.example.ApiTrabajador.Modelos.TrabajadorModelo;
 import com.example.ApiTrabajador.Servicios.trabajadorServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE})
 @RequestMapping("/trabajador")
 public class TrabajadorControlador {
 
